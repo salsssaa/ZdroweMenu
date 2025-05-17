75b08501160e9fefb0bd60af07853a5b3c824902
salsa <nowakkrystian008@gmail.com>
2025-05-17 16:13:24 +0200

    app.js

--- zdrowemenu/src/App.js
+++ zdrowemenu/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    const fetchdata = async ()=>{
+    const result = await fetch("http://localhost:8000/api/product/3017624010701?fields=product_name,nutriscore_data")
+    const data = await result.json()
+    console.log(data)
+    }
+    fetchdata()
+  },[])
+  if (!data) return <p>Loading...</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{data.product?.product_name}</h2>
+      <pre>{JSON.stringify(data.product?.nutriscore_data, null, 2)}</pre>
     </div>
   );
 }
